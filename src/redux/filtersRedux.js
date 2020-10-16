843d79be32d7feeff0b62eef9a4c210beb8a2b09
Joanna Stachowicz <joannastachowicz19@gmail.com>
2020-10-16 23:25:42 +0200

    Implement routing and redux

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -1,6 +1,6 @@
 /* SELECTORS */
 
-export const getAllFilters = ({filters}) => filters;
+export const getAllFilters = ({ filters }) => filters;
 
 /* ACTIONS */
 
@@ -10,11 +10,15 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
-// TODO - add other action types
+export const HANDLE_DURATION = createActionName('HANDLE_DURATION');
+export const ADD_TAG = createActionName('ADD_TAG');
+export const REMOVE_TAG = createActionName('REMOVE_TAG');
 
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
-// TODO - add other action creators
+export const handleDuration = payload => ({ payload, type: HANDLE_DURATION });
+export const addTag = payload => ({ payload, type: ADD_TAG });
+export const removeTag = payload => ({ payload, type: REMOVE_TAG });
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
@@ -24,7 +28,53 @@ export default function reducer(statePart = [], action = {}) {
         ...statePart,
         searchPhrase: action.payload,
       };
-    // TODO - handle other action types
+
+    case HANDLE_DURATION:
+      if (action.payload.type == 'from') {
+        let changedFrom = parseInt(action.payload.value);
+        if (changedFrom > statePart.duration.to) {
+          changedFrom = statePart.duration.to;
+        }
+        return {
+          ...statePart,
+          duration: {
+            from: changedFrom,
+            to: statePart.duration.to,
+          },
+        };
+      }
+      else if (action.payload.type == 'to') {
+        let changedTo = parseInt(action.payload.value);
+        if (changedTo < statePart.duration.from) {
+          changedTo = statePart.duration.from;
+        }
+        return {
+          ...statePart,
+          duration: {
+            to: changedTo,
+            from: statePart.duration.from,
+          },
+        };
+      }
+      break;
+
+    case ADD_TAG: {
+      let newState = {
+        ...statePart,
+      };
+      newState.tags.push(action.payload);
+      return newState;
+    }
+
+    case REMOVE_TAG: {
+      let anotherState = {
+        ...statePart,
+      };
+      let index = anotherState.tags.indexOf(action.payload);
+      anotherState.tags.splice(index, 1);
+      return anotherState;
+    }
+
     default:
       return statePart;
   }
--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -1,41 +1,37 @@
 /* SELECTORS */
 
-export const getAllTrips = ({trips}) => trips;
+export const getAllTrips = ({ trips }) => trips;
 
-export const getFilteredTrips = ({trips, filters}) => {
+export const getFilteredTrips = ({ trips, filters }) => {
   let output = trips;
 
   // filter by search phrase
-  if(filters.searchPhrase){
+  if (filters.searchPhrase) {
     const pattern = new RegExp(filters.searchPhrase, 'i');
     output = output.filter(trip => pattern.test(trip.name));
   }
 
-  // TODO - filter by duration
+  // filter by duration
+  output = output.filter(trip => trip.days >= filters.duration.from && trip.days <= filters.duration.to);
 
-  // TODO - filter by tags
+  let checker = (arr, target) => target.every(v => arr.includes(v));
+  output = output.filter(trip => checker(trip.tags, filters.tags));
 
-  // TODO - sort by cost descending (most expensive goes first)
+  // sort by cost descending (most expensive goes first)
+  output = output.sort((a, b) => Number(a.cost.replace(/[^0-9.-]+/g, '')) - Number(b.cost.replace(/[^0-9.-]+/g, '')));
+  output = output.reverse();
 
   return output;
 };
 
-export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
-
-  // TODO - filter trips by tripId
-
-  console.log('filtering trips by tripId:', tripId, filtered);
-  return filtered.length ? filtered[0] : {error: true};
+export const getTripById = ({ trips }, tripId) => {
+  const filtered = trips.filter(trip => (trip.id == tripId));
+  return filtered.length ? filtered[0] : { error: true };
 };
 
-export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
-
-  // TODO - filter trips by countryCode
-
-  console.log('filtering trips by countryCode:', countryCode, filtered);
-  return filtered.length ? filtered : [{error: true}];
+export const getTripsForCountry = ({ trips }, countryCode) => {
+  const filtered = trips.filter(trip => (trip.country.code == countryCode));
+  return filtered.length ? filtered : [{ error: true }];
 };
 
 /* ACTIONS */
