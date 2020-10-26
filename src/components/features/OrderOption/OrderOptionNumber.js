59c045129bd9bba42f1f935afc3f9bdf980c7e91
Joanna Stachowicz <joannastachowicz19@gmail.com>
2020-10-26 13:23:37 +0100

    Modify OrderOptionNumber component

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -5,20 +5,25 @@ import styles from './OrderOption.scss';
 
 import { formatPrice } from '../../../utils/formatPrice';
 
-const OrderOptionNumber = ({ currentValue, limits, tripCost, price, setOptionValue }) => (
-  <div className={styles.number}>
-    <input
-      className={styles.inputSmall}
-      type="number"
-      value={currentValue}
-      min={limits.min}
-      max={limits.max}
-      onChange={event => setOptionValue(event.currentTarget.value)}
-    >
-    </input>
-    ({ formatPrice(Number(tripCost.replace(/[^0-9.-]+/g, '')) * parseFloat(currentValue) * parseFloat(price) / parseFloat(100))})
-  </div >
-);
+const OrderOptionNumber = ({ currentValue, limits, tripCost, price, setOptionValue }) => {
+  const tripCostValue = Number(tripCost.replace(/[^0-9.-]+/g, ''));
+  const currentValueNumber = parseFloat(currentValue);
+  const priceNumber = parseFloat(price) / parseFloat(100);
+  return (
+    <div className={styles.number}>
+      <input
+        className={styles.inputSmall}
+        type="number"
+        value={currentValue}
+        min={limits.min}
+        max={limits.max}
+        onChange={event => setOptionValue(event.currentTarget.value)}
+      >
+      </input>
+    ({ formatPrice(tripCostValue * currentValueNumber * priceNumber)})
+    </div >
+  );
+};
 
 OrderOptionNumber.propTypes = {
   limits: PropTypes.object,
